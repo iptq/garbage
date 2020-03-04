d154536f6ec500fc5c6602564fb50c5b47f898f6
Michael Zhang <iptq@protonmail.com>
2020-03-04 00:19:31 -0600

    ouais

--- src/ops/restore.rs
+++ src/ops/restore.rs
@@ -54,6 +54,7 @@ pub fn restore(options: RestoreOptions) -> Result<()> {
         Ok(i) if i < files.len() => {
             let info = files.get(i).unwrap();
             eprintln!("moving {:?} to {:?}", &info.deleted_path, &info.path);
+            fs::remove_file(&info.info_path)?;
             fs::rename(&info.deleted_path, &info.path)?;
         }
         _ => eprintln!("Invalid number."),
